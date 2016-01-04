c76218719b63236e42a0de9fe6196364f74a545b
Peter Cho <pcho@phase2technology.com>
2016-01-03 23:34:21 -0500

    Used jquery ready handler to ensure ads gets loaded after mmenu

--- theme/dfp_tag.tpl.php
+++ theme/dfp_tag.tpl.php
@@ -10,13 +10,15 @@
     print drupal_render($slug);
   endif; ?>
   <script type="text/javascript">
-    googletag.cmd.push(function() {
-      googletag.display("<?php print $tag->placeholder_id ?>");
-      <?php // TODO: First, move this forked module into the repo. ?>
-      <?php // TODO: Second, the second parameter here could be less tacky.... ?>
-      <?php if (variable_get('dfp_disable_init_load', 0) && $tag->machinename != 'block_gallery'): ?>
-googletag.pubads().refresh([dfp_entry.<?php print $tag->machinename; ?>]);
-      <?php endif; ?>
+    $(document).ready(function() {
+      googletag.cmd.push(function() {
+        googletag.display("<?php print $tag->placeholder_id ?>");
+        <?php // TODO: First, move this forked module into the repo. ?>
+        <?php // TODO: Second, the second parameter here could be less tacky.... ?>
+        <?php if (variable_get('dfp_disable_init_load', 0) && $tag->machinename != 'block_gallery'): ?>
+  googletag.pubads().refresh([dfp_entry.<?php print $tag->machinename; ?>]);
+        <?php endif; ?>
+      });
     });
   </script>
 </div>
